7eea4f9139d4e1c636775a2f1a05412478f38d07
kevdtian <38590861+kevdtian@users.noreply.github.com>
2023-02-05 20:05:31 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -24,89 +24,100 @@ class MyGUI:
         # Create the main frame and assign it the root variable
         self.root = tk.Tk()
 
-        # Set the resolution of root
-        self.root.geometry("1920x1080")
-        # Setr the background of root to gray
+        # Set the resolution and center root
+        w = 1280
+        h = 720
+        ws = self.root.winfo_screenwidth()
+        hs = self.root.winfo_screenheight()
+        x = (ws/2) - (w/2)
+        y = (hs/2) - (h/2)
+
+        self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+        # Set the background of root to gray
         self.root.configure(bg="#212124")
+
         # Set the title of root
         self.root.title("Auth Proxy Log Parser")
 
-        # Create the first frame inside of root and assign it the topframe variable
-        self.topframe = tk.Frame(self.root, width=50, height=40, bg="#45ba6e")
-        self.topframe.pack(fill="x", pady=5)
+        # Create the first frame inside of root and assign it the topFrame variable
+        self.topFrame = tk.Frame(self.root, width=50, height=40, bg="#45ba6e")
+        self.topFrame.pack(fill="x", pady=5)
 
-        # Create the second frame inside of root and assign it the leftframe variable
-        self.leftframe = tk.Frame(self.root, width=500, height=500, bg="#212124")
-        self.leftframe.pack(
+        # Create the second frame inside of root and assign it the leftFrame variable
+        self.leftFrame = tk.Frame(self.root, width=500, height=500, bg="#212124")
+        self.leftFrame.pack(
             pady=5, padx=5, ipady=5, ipadx=5, side="left", expand=True, fill="both"
         )
 
-        # Create the third frame inside of root and assign it the rightframe variable
+        # Create the third frame inside of root and assign it the rightFrame variable
 
-        self.rightframe = tk.Frame(self.root, width=500, height=500, bg="#212124")
-        self.rightframe.pack(ipady=5, ipadx=5, side="left", expand=True, fill="both")
+        self.rightFrame = tk.Frame(self.root, width=500, height=500, bg="#212124")
+        self.rightFrame.pack(ipady=5, ipadx=5, side="left", expand=True, fill="both")
 
-        # Create the text above output field (Label Object) and assing it the label variable
+        # Create the text above output field (Label Object) and adding it the label variable
         self.clear_button = tk.Button(
-            self.rightframe, text="Clear", command=self.clear, width=4, bg="#212124"
+            self.rightFrame, text="Clear", command=self.clear, width=4, bg="#212124", fg="#FAF9F6"
         )
         self.clear_button.pack(side="top", anchor="w", padx=5, pady=5)
 
-        # Create the text inside the topframe  (Label Object) and assing it the label variable
+        # Create the text inside the topFrame  (Label Object) and adding it the label variable
         self.label = tk.Label(
-            self.topframe,
+            self.topFrame,
             text="Duo Security Authentication Proxy Log Parser",
             bg="#45ba6e",
             font=("ariel", 25),
         )
         self.label.grid(row=30, column=2)
 
-        # Create the text above the serach box (Label Object) and assing it the textboxlabel variable
-        self.textboxlabel = tk.Label(
-            self.leftframe,
-            text="Search string",
-            font=("Times New Roman", 20),
+        # Create the text above the search box (Label Object) and adding it the textboxLabel variable
+        self.textboxLabel = tk.Label(
+            self.leftFrame,
+            text="Search string:",
+            font=("Arial", 17),
             bg="#212124",
+            fg="#FAF9F6",
         )
-        self.textboxlabel.pack(anchor="nw", padx=5)
+        self.textboxLabel.pack(anchor="nw", padx=5)
 
-        # Create the search box (Entry object) and assing it the textbox variable
+        # Create the search box (Entry object) and adding it the textbox variable
         self.textbox = tk.Entry(
-            self.leftframe, width=40, font=("Times New Roman", 20), bg="#212124"
+            self.leftFrame, width=20, font=("Arial", 20), bg="#212124"
         )
         self.textbox.pack(anchor="n", expand=False, fill="x", padx=5)
 
         # Create a text object box with the appropriate sizing. Set the status to disabled (non-editable)
-        self.outputbox = tk.Text(
-            self.rightframe,
+        self.outputBox = tk.Text(
+            self.rightFrame,
             width=50,
-            font=("Times New Roman", 20),
+            font=("Arial", 12),
             wrap="word",
             state=tk.DISABLED,
             bg="#212124",
+            fg="#FAF9F6",
         )
 
         # Pack the Text object
-        self.outputbox.pack(expand=True, fill="both", padx=5)
+        self.outputBox.pack(expand=True, fill="both", padx=5)
 
-        # Create quitbutton (Button Object) for the quit button. Assign frame,text, command, and pack it. Best practice is to seperate the packing from the variable (to do)
-        self.quitbutton = tk.Button(
-            self.leftframe, text="Quit", command=self.root.destroy, bg="#212124"
+        # Create quitButton (Button Object) for the quit button. Assign frame,text, command, and pack it. Best practice is to separate the packing from the variable (to do)
+        self.quitButton = tk.Button(
+            self.leftFrame, text="Quit", command=self.root.destroy, bg="#212124", fg="#FAF9F6"
         )
-        self.quitbutton.pack(side="bottom", anchor="sw")
+        self.quitButton.pack(side="bottom", anchor="sw")
 
-        # Create the show button (Button object) and assing it the show variable
-        self.show = tk.Button(
-            self.leftframe, text="Search", command=None, width=4, bg="#212124"
+        # Create the search button (Button object) and adding it the searchButton variable
+        self.searchButton = tk.Button(
+            self.leftFrame, text="Search", command=None, width=7, bg="#212124", fg="#FAF9F6"
         )
-        self.show.pack(anchor="nw", after=self.textbox)
+        self.searchButton.pack(anchor="nw", padx=5, pady=10, after=self.textbox)
 
-        # Create the date button (Button Object) and assign it the datebutton variable
-        self.datebutton = tk.Button(
-            self.leftframe, text="Date", command=self.show_date, width=4, bg="#212124"
+        # Create the date button (Button Object) and assign it the dateButton variable
+        self.dateButton = tk.Button(
+            self.leftFrame, text="Date", command=self.show_date, width=7, bg="#212124", fg="#FAF9F6"
         )
-        self.datebutton.pack(side="top", anchor="w")
-        self.current_date_label = tk.Label(self.leftframe, text=self.date, bg="#212124")
+        self.dateButton.pack(side="top", anchor="w", padx=5)
+        self.current_date_label = tk.Label(self.leftFrame, text=self.date, bg="#212124", fg="#FAF9F6")
         self.current_date_label.pack()
         self.display_output_box(conc_file, None, None)
 
@@ -114,36 +125,36 @@ class MyGUI:
         self.root.mainloop()
 
     def display_output_box(self, words, from_date, to_date):
-        """Inserts/prints the values entered into textbox into the output box. Changes the status of the outputbox to normal, enters the value, then changes the
+        """Inserts/prints the values entered into textbox into the output box. Changes the status of the outputBox to normal, enters the value, then changes the
         status to disabled.
         input: str. What to be displayed in the output box.
         return: None
         """
-        self.outputbox.configure(state=tk.NORMAL)
-        self.outputbox.delete(1.0, "end")
+        self.outputBox.configure(state=tk.NORMAL)
+        self.outputBox.delete(1.0, "end")
         if from_date and to_date:
             words = filter_by_date(words, from_date, to_date)
 
         if words:
             # insert the input from the input textbox (Entry object) at line 1, character 0
             for line in range(len(words)):
-                self.outputbox.insert(1.0, words[line])
-            # self.outputbox.insert(1.0, words)
+                self.outputBox.insert(1.0, words[line])
+            # self.outputBox.insert(1.0, words)
             # Change the status of the text object to DISABLED (non editable)
-            self.outputbox.configure(state=tk.DISABLED)
+            self.outputBox.configure(state=tk.DISABLED)
         else:
-            self.outputbox.insert(
+            self.outputBox.insert(
                 1.0,
                 "No files found in default directories, please import Proxy log files.",
             )
-            # self.outputbox.insert(1.0, words)
+            # self.outputBox.insert(1.0, words)
             # Change the status of the text object to DISABLED (non editable)
-            self.outputbox.configure(state=tk.DISABLED)
+            self.outputBox.configure(state=tk.DISABLED)
 
     def clear(self):
-        self.outputbox.configure(state=tk.NORMAL)
-        self.outputbox.delete(1.0, "end")
-        self.outputbox.configure(state=tk.DISABLED)
+        self.outputBox.configure(state=tk.NORMAL)
+        self.outputBox.delete(1.0, "end")
+        self.outputBox.configure(state=tk.DISABLED)
 
     def grab_search_box(self):
         """returns the string inputted in the search box of the gui.
@@ -155,8 +166,8 @@ class MyGUI:
         """Initializes a new frame for the date and time entry.
         args: None
         return: None"""
-        self.datebutton.configure(state=tk.DISABLED)
-        self.from_frame = tk.Frame(self.leftframe, width=50, height=80)
+        self.dateButton.configure(state=tk.DISABLED)
+        self.from_frame = tk.Frame(self.leftFrame, width=50, height=80)
         self.from_label = tk.Label(self.from_frame, text="From")
         self.from_date = Calendar(
             self.from_frame,
@@ -167,7 +178,7 @@ class MyGUI:
         self.submit_button = tk.Button(
             self.from_frame, text="Submit", command=self.get_date_range
         )
-        self.to_frame = tk.Frame(self.leftframe, width=50, height=80)
+        self.to_frame = tk.Frame(self.leftFrame, width=50, height=80)
         self.to_label = tk.Label(self.to_frame, text="To")
         self.to_date = Calendar(
             self.to_frame,
@@ -184,7 +195,7 @@ class MyGUI:
         self.to_date.pack(anchor="w")
 
     def get_date_range(self):
-        self.datebutton.configure(state=tk.NORMAL)
+        self.dateButton.configure(state=tk.NORMAL)
         self.date = self.from_date.get_date(), "to", self.to_date.get_date()
         self.from_frame.destroy()
         self.to_frame.destroy()
@@ -194,7 +205,7 @@ class MyGUI:
 
     def update_shown_date(self):
         self.current_date_label.destroy()
-        self.current_date_label = tk.Label(self.leftframe, text=self.date)
+        self.current_date_label = tk.Label(self.leftFrame, text=self.date)
         self.current_date_label.pack()
 
 
@@ -227,14 +238,14 @@ def import_default_dir():
 def filter_log_directory(path, start_with):
     """Filter the items in the provided list to ensure that they start with the appropriate string.
     args: list, path of the log folder. str, the string to filter by (what the files begin with). (case sensitive).
-    retur: list, list of files that start with the given argument"""
+    return: list, list of files that start with the given argument"""
     return [file for file in path if file.startswith(start_with)]
 
 
 def merge_files(path, files):
     """Merge the provided proxy log files into one file object. Files are sorted in order from newest to oldest
     args: str, path of the files, list, list of the file names
-    return: list. lines of concatinated files"""
+    return: list. lines of concatenated files"""
     return_list = []
     files.sort()
     for file in files:
@@ -287,7 +298,7 @@ def calculate_date_difference(from_date, to_date):
     return list_of_dates
 
 
-"""Add logic to detect 'Choose File' button value. If none is chosen, run the below funciton call"""
+"""Add logic to detect 'Choose File' button value. If none is chosen, run the below function call"""
 # Call on the import_default_dir function. Assign the two return values to directory and conc file. Note that at the moment, the value for directory
 # is not used, but may become useful in the future.
 directory, conc_file = import_default_dir()
