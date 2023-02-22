2927e34a0a7fdff5fb97e1c2835925e2f4ca3c3c
mdpocse <mdpocse@gmail.com>
2023-02-22 12:55:26 -0500

    Recolored buttons and also introduced dictionary Kbs (partially complete.)

--- gui.py
+++ gui.py
@@ -86,44 +86,40 @@ class MyGUI(tk.Frame):
 
         # Create the first frame inside of root and assign it the topFrame variable
         self.topFrame = tk.Frame(self.root, width=50, height=40, bg='#45ba6e')
-        self.topFrame.pack(fill='x', pady=5)
 
         # Create the second frame inside of root and assign it the leftFrame variable
         self.leftFrame = tk.Frame(self.root, width=500, height=500, bg="#212124")
-        self.leftFrame.pack(pady=5, padx=5, ipady=5, ipadx=5, side='left', fill='both')
 
         # Create the third frame inside of root and assign it the rightFrame variable
-
         self.rightFrame = tk.Frame(self.root, width=500, height=500, bg="#212124")
-        self.rightFrame.pack(ipady=5, ipadx=5, side="left", expand=True, fill="both")
 
+        # Create the bottom right frame inside of the right frame and assign it the rightFrame variable
         self.bottomRightFrame = tk.Frame(self.rightFrame, width=50, height=20, bg="#212124")
-        self.bottomRightFrame.pack(
-            ipady=5, ipadx=5, side="bottom", expand=True, fill="both"
-        )
 
+        # Create the bottom left frame inside of the left frame and assign it the rightFrame variable
         self.bottomLeftFrame = tk.Frame(self.leftFrame, width=50, height=275, bg="#212124")
-        self.bottomLeftFrame.pack(ipady=5, ipadx=5, side="bottom", fill='both', expand=True)
 
-        # Create the text above output field (Label Object) and add it the label variable
-        self.reset_button = tk.Button(
-            self.rightFrame, text="Reset to Default", command=self.reset, highlightcolor="#212124", highlightbackground="#212124", bg="#212124", fg="#FAF9F6"
-        )
-        self.reset_button.pack(side="left", anchor="w", pady=5)
+        self.compileFrameLeft = tk.Frame(self.bottomLeftFrame, height=275, bg="red")
 
-        self.choose_file = tk.Button(
-            self.rightFrame, text="Choose File", command=self.open_file, highlightcolor="#212124", highlightbackground="#212124", bg="#212124", fg="#FAF9F6"
-        )
-        self.choose_file.pack(side="left", anchor="w", padx=5)
+        self.compileFrameRight = tk.Frame(self.bottomLeftFrame, height=275, bg="blue")
 
-        # Create the text inside the topFrame  (Label Object) and adding it the label variable
+        # Create the reset button
+        self.reset_button = tk.Button(self.rightFrame, text="Reset to Default", command=self.reset,
+                                      highlightcolor="#212124", highlightbackground="#212124", bg="#212124",
+                                      fg="#FAF9F6")
+
+        # Create the choose file button
+        self.choose_file = tk.Button(self.rightFrame, text="Choose File", command=self.open_file,
+                                     highlightcolor="#212124", highlightbackground="#212124", bg="#212124",
+                                     fg="#FAF9F6")
+
+        # Create the text inside the topFrame  (Label Object) and add it the label variable
         self.label = tk.Label(
             self.topFrame,
             text="Duo Security Authentication Proxy Log Parser",
             bg="#45ba6e",
             font=("ariel", 25),
         )
-        self.label.grid(row=30, column=2)
 
         # Create the text above the search box (Label Object) and adding it the textboxLabel variable
         self.textboxLabel = tk.Label(
@@ -134,23 +130,17 @@ class MyGUI(tk.Frame):
             fg="#FAF9F6",
         )
 
-        self.textboxLabel.pack(anchor="nw", padx=5)
-
         # Create the search box (Entry object) and adding it the textbox variable
-        #ttk.Style().configure('pad.TEntry', padding='5 1 1 1', insertcolor='#FAF9F6')
-
         self.textbox = tk.Entry(
-            self.leftFrame, width=47, font=("Arial", 16), insertbackground="#FAF9F6", bg="#212124", fg="#FAF9F6", justify='left'
+            self.leftFrame, width=47, font=("Arial", 16), insertbackground="#FAF9F6", bg="#212124", fg="#FAF9F6",
+            justify='left'
         )
-        
         self.textbox.bind("<Return>", self.search_input)
 
-        self.textbox.pack(anchor="n", expand=False, fill="x", padx=5)
-
         # Create a text object box with the appropriate sizing. Set the status to disabled (non-editable)
-        self.scrollBar = Scrollbar(self.bottomRightFrame, orient='vertical', bg="#212124", troughcolor="#212124", highlightbackground="#212124", jump=0)
-        self.scrollBar.pack(side='right', fill='y')
-        
+        self.scrollBar = Scrollbar(self.bottomRightFrame, orient='vertical', bg="#212124", troughcolor="#212124",
+                                   highlightbackground="#212124", jump=0)
+
         self.outputBox = CustomText(
             self.bottomRightFrame,
             width=50,
@@ -162,37 +152,49 @@ class MyGUI(tk.Frame):
             yscrollcommand=self.scrollBar.set
         )
         self.scrollBar.config(command=self.outputBox.yview)
-        # Pack the Text object
-        self.outputBox.pack(expand=True, fill="both", side="left", anchor="w", padx=1, pady=1)
+
         # Create quitButton (Button Object) for the quit button. Assign frame,text, command, and pack it.
         self.quitButton = tk.Button(
-            self.leftFrame, text="Quit", command=self.root.destroy, highlightcolor="#212124", highlightbackground="#212124", bg="#212124", fg="#FAF9F6"
+            self.leftFrame, text="Quit", command=self.root.destroy, highlightcolor="#212124",
+            highlightbackground="#212124", bg="#212124", fg="#FAF9F6"
         )
-        self.quitButton.pack(side="bottom", anchor="sw", before=self.bottomLeftFrame)
-
-        
-        #self.lineCount = tk.Spinbox(self.leftFrame, from_=1, to_=9, width=5)
-        #self.lineCount.pack(anchor="nw", padx=5, ipady=1)
 
         # Create the search button (Button object) and adding it the searchButton variable
-        
         self.searchButton = tk.Button(
-            self.leftFrame, text="Search", command=self.search_input, width=7, highlightcolor="#212124", highlightbackground="#212124", bg="#212124", fg="#FAF9F6"
+            self.leftFrame, text="Search", command=self.search_input, width=7, highlightcolor="#212124",
+            highlightbackground="#212124", bg="#212124", fg="#FAF9F6"
         )
-        self.searchButton.pack(anchor="nw", padx=5, pady=10)
 
         # Create the date button (Button Object) and assign it the dateButton variable
         self.dateButton = tk.Button(
-            self.leftFrame, text="Date", command=self.show_date, width=7, highlightcolor="#212124", highlightbackground="#212124", bg="#212124", fg="#FAF9F6"
+            self.leftFrame, text="Date", command=self.show_date, width=7, highlightcolor="#212124",
+            highlightbackground="#212124", bg="#212124", fg="#FAF9F6"
         )
-        self.dateButton.pack(anchor="nw", padx=5)
-        
-        self.current_date_label = tk.Label(self.leftFrame, text=None, highlightcolor="#212124", highlightbackground="#212124", bg="#212124", fg="#FAF9F6")
-        self.current_date_label.pack(anchor='w', padx=1)
+
+        self.current_date_label = tk.Label(self.leftFrame, text=None, highlightcolor="#212124",
+                                           highlightbackground="#212124", bg="#212124", fg="#FAF9F6")
 
         self.errorsButton = tk.Button(
-            self.bottomLeftFrame, text="Compile Errors", command=self.init_compiler, width=12, highlightcolor="#2c6fbb", highlightbackground="#2c6fbb",bg='#2c6fbb'
+            self.bottomLeftFrame, text="Compile Errors", command=self.init_compiler, width=12, highlightcolor="#2c6fbb",
+            highlightbackground="#2c6fbb", bg='#2c6fbb'
         )
+
+        self.topFrame.pack(fill='x', pady=5)
+        self.leftFrame.pack(pady=5, padx=5, ipady=5, ipadx=5, side='left', fill='both')
+        self.rightFrame.pack(ipady=5, ipadx=5, side="left", expand=True, fill="both")
+        self.bottomRightFrame.pack(ipady=5, ipadx=5, side="bottom", expand=True, fill="both")
+        self.bottomLeftFrame.pack(ipady=5, ipadx=5, side="bottom", fill='both', expand=True)
+        self.reset_button.pack(side="left", anchor="w")
+        self.choose_file.pack(side="left", anchor="w", padx=5)
+        self.label.grid(row=30, column=2)
+        self.textboxLabel.pack(anchor="nw", padx=5)
+        self.textbox.pack(anchor="n", expand=False, fill="x", padx=5)
+        self.scrollBar.pack(side='right', fill='y')
+        self.outputBox.pack(expand=True, fill="both", side="left", anchor="w", padx=1, pady=1)
+        self.quitButton.pack(side="bottom", anchor="sw", before=self.bottomLeftFrame)
+        self.searchButton.pack(anchor="nw", padx=5, pady=10)
+        self.dateButton.pack(anchor="nw", padx=5)
+        self.current_date_label.pack(anchor='w', padx=50)
         self.errorsButton.pack(anchor='e')
 
 
@@ -264,6 +266,8 @@ class MyGUI(tk.Frame):
 
     def reset(self):
         self.outputBox.configure(state=tk.NORMAL)
+        self.textbox.delete(0, 'end')
+        print(self.textbox.get())
         self.concatenated_file = self.default_file
         self.current_date_label.destroy()
         self.date = "Any", "to", "Any"
@@ -370,67 +374,80 @@ class MyGUI(tk.Frame):
  
     def init_compiler(self):
         self.errorsButton.configure(command=self.destroy_event_compiler)
-        button_names, lines = compile_events(self.concatenated_file)
-
+        self.compileFrameLeft.pack(side='left', fill='x')
+        self.compileFrameRight.pack(side='right', fill='x')
+        button_names, lines, kbs = compile_events(self.concatenated_file)
+        print(kbs)
         for index in range(len(button_names)):
             if len(lines[index]) > 0:
-                self.name = tk.Button(self.bottomLeftFrame,
+                self.name = tk.Button(self.compileFrameLeft,
                                       command=partial(self.display_output_box, lines[index], None, None),
-                                      text=button_names[index], width=20)
+                                      text=button_names[index], width=21)
                 self.name.pack(anchor='nw')
+                if len(kbs[index]) > 0:
+                    self.kb = tk.Button(self.compileFrameRight,
+                                          command=None,
+                                          text=kbs[index], width=30)
+                    self.kb.pack()
 
     def destroy_event_compiler(self):
         self.errorsButton.configure(command=self.init_compiler)
-        objects_in_frame = self.bottomLeftFrame.winfo_children()
+        self.compileFrameRight.destroy()
+        self.compileFrameLeft.destroy()
+        self.compileFrameLeft = tk.Frame(self.bottomLeftFrame, height=275, bg="red")
+        self.compileFrameRight = tk.Frame(self.bottomLeftFrame, height=275, bg="blue")
+        '''objects_in_frame = self.compileFrameLeftLeftFrame.winfo_children()
+        kbs_in_frame = self.compileFrameLeftLeftFrame.winfo_children()
         for obj in objects_in_frame:
             if obj.cget('text') != "Compile Errors":
                 obj.destroy()
             elif obj.cget('text') == 'Compile Errors':
                 obj.configure(command=self.init_compiler)
 
-
+'''
 def compile_events(conc_file):
     # The dictionary format is: {KEY(str)    name of button:
     #                           VALUE(list)      [(error strs),[line log file]]}
-    events_dic = {'Successful Authentication': (('Success, Logging you in',), []),
-                  'Incorrect Password': (('invalidCredentials', 'data 52e',), []),
-                  'Invalid SKEY': (('Invalid SKEY',), []),
-                  'No User LDAP permission': (('data 531',), []),
-                  'Auth Proxy Start': (('Duo Security Authentication Proxy', 'Init Complete',), []),
-                  'Disabled AD User': (('data 533', 'data534'), []),
-                  'Locked AD Account': (('data 775',), []),
+    events_dic = {'Successful Authentication': (('Success, Logging you in',), [], ()),
+                  'Incorrect Password': (('invalidCredentials', 'data 52e',), [], ()),
+                  'Invalid SKEY': (('Invalid SKEY',), [], ('Check the Skey')),
+                  'No User LDAP permission': (('data 531',), [], ('Bad user worstation permission')),
+                  'Auth Proxy Start': (('Duo Security Authentication Proxy', 'Init Complete',), [], ('')),
+                  'Disabled AD User': (('data 533', 'data534'), [], ()),
+                  'Locked AD Account': (('data 775',), [], ()),
                   'Service Account no bind': (('Error sending AD auth request', 'invalid Credentials,',),
-                                                           []),
+                                                           [], ('test3')),
                   'Bind after disconnect': (
-                      ('Attempt to bindRequest multiple times in the same LDAP connection',), []),
-                  'Invalid RADIUS IP': (('dropping packet', 'Unknown Client',), []),
-                  'Shared secret mismatch': (('Cannot decode password',), []),
-                  'Invalid encrypted secret': (('Invalid secret',), []),
+                      ('Attempt to bindRequest multiple times in the same LDAP connection',), [], ()),
+                  'Invalid RADIUS IP': (('dropping packet', 'Unknown Client',), [], ()),
+                  'Shared secret mismatch': (('Cannot decode password',), [], ()),
+                  'Invalid encrypted secret': (('Invalid secret',), [], ()),
                   'Primary auth disconnect': (('User timeout caused connection failure',),
-                                                                         []),
-                  'DNS failing': (('DNS lookup failed',), []),
+                                                                         [], ()),
+                  'DNS failing': (('DNS lookup failed',), [], ()),
                   'DNS failing secure': (
-                      ('DNSLookupError', 'Denied Duo login on preauth failure'), []),
+                      ('DNSLookupError', 'Denied Duo login on preauth failure'), [], ()),
                   'Cloud unreachable TCP': (
-                      ('TCPTimedOutError', 'allowed Duo login on preauth failure'), []),
-                  'Invalid protected SKEY': (('CryptUnprotectData', 'The data is invalid',), []),
-                  'Invalid IKEY': (('Invalid integration key in request credentials', '40102'), []),
-                  'Invalid private key file': (('Could not open private key file',), []),
+                      ('TCPTimedOutError', 'allowed Duo login on preauth failure'), [], ()),
+                  'Invalid protected SKEY': (('CryptUnprotectData', 'The data is invalid',), [], ()),
+                  'Invalid IKEY': (('Invalid integration key in request credentials', '40102'), [], ()),
+                  'Invalid private key file': (('Could not open private key file',), [], ()),
                   'User not in sec group': (
-                      ('Tried to search security group DN for object sid but it could not be found',), []),
-                  'User not enrolled block': (('enroll', 'enrolled'), []),
-                  'No reply from user': (('Login timed out',), []),
-                  'User not enrolled pass': (('Allowing unknown user',), []),
-                  'Plain auth required': (('0xA3',), []),
+                      ('Tried to search security group DN for object sid but it could not be found',), [], ()),
+                  'User not enrolled block': (('enroll', 'enrolled'), [], ()),
+                  'No reply from user': (('Login timed out',), [], ()),
+                  'User not enrolled pass': (('Allowing unknown user',), [], ()),
+                  'Plain auth required': (('0xA3',), [], ()),
                   'Bad RADIUS protocol': (
-                      ('Missing or improperly formatted password',), []),
-                  'User not found': (('data 525',), []),
-                  'User PW Expired': (('data 532',), []),
-                  'User PW Reset': (('data 773',), []),
-                  'unlike hosts AD sync': (('sections must have the exact same',), []),
-                  'NTLMv2 disabled on DC': (('data 1',), []),
-                  'Invalid mail attribute': (('Invalid LDAP filter',), [])
+                      ('Missing or improperly formatted password',), [], ()),
+                  'User not found': (('data 525',), [], ()),
+                  'User PW Expired': (('data 532',), [], ()),
+                  'User PW Reset': (('data 773',), [], ()),
+                  'unlike hosts AD sync': (('sections must have the exact same',), [], ()),
+                  'NTLMv2 disabled on DC': (('data 1',), [], ()),
+                  'Invalid mail attribute': (('Invalid LDAP filter',), [], ())
                   }
+
     for line in conc_file:
         for values in events_dic.values():
             for search_str in values[0]:
@@ -438,9 +455,9 @@ def compile_events(conc_file):
                     values[1].append(line)
 
     error_lines = [error[1] for error in events_dic.values()]
+    kb_lines = [kb[2] for kb in events_dic.values()]
 
-    return list(events_dic.keys()), error_lines
-
+    return list(events_dic.keys()), error_lines, kb_lines
 
 def import_default_dir():
     """Search through program files and program files x86. If the auth proxy directory is found in either, filter the log folder (to only include
